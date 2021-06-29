4509f6c1bc7a102721b30a05abae9069b2faf12c
Joshua Schmid <jaiks@posteo.de>
2021-06-29 17:31:00 +0200

    tmp

--- dev/pythonkafka.py
+++ dev/pythonkafka.py
@@ -1,7 +1,7 @@
 from pykafka import KafkaClient, SslConfig
 
 
-cfg = SslConfig(cafile='truststore/ca-cert', keyfile='keystore/combined.key', password='confluent')
+cfg = SslConfig(cafile='truststore/ca-cert', certfile='keystore/combined.key', password='confluent')
 
 client = KafkaClient(hosts="localhost:9093", ssl_config=cfg)
 
--- dev/test-mtls.sh
+++ dev/test-mtls.sh
@@ -7,4 +7,4 @@ openssl pkcs12 -in keystore/client.keystore.jks -out keystore/combined.key -pass
 echo "Showing public key"
 openssl x509 -in keystore/combined.key -text -noout
 echo "Test connection with client key"
-openssl s_client -connect localhost:9093 -CAfile truststore/ca-cert -key keystore/combined.key -state -debug -pass pass:$PASS
+openssl s_client -connect localhost:9093 -CAfile truststore/ca-cert -cert keystore/combined.key -state -debug -pass pass:$PASS
